68cd6e76a239263df5827e59b7c3289fab3db233
Nurlan Ormond <sunshineforester@gmail.com>
2023-03-21 01:11:01 +0300

    Program is written

--- check_work/Program.cs
+++ check_work/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+String [] GetWords ()
+{
+   String [] text = new String [4]; 
+    System.Console.WriteLine("\n\nInput 4 Words:");
+    for (int i = 0; i < text.Length; i++)
+    {
+        text [i] = Console.ReadLine();
+    }
+    return text;
+
+}
+
+String [] ResultArray (String [] array)
+{
+    String [] resultArray = new String [4];
+    for (int i = 0, j =0; i < array.Length; i++)
+    {
+        if (array[i].Length <=3)
+        {
+            resultArray[j] = array [i];
+            j++;    
+
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray (String [] array)
+{
+    System.Console.Write("\nHere are words containing 3 symbols: ");
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]+" ");
+    }
+    System.Console.WriteLine("\n\n");
+}
+
+PrintArray (ResultArray(
+            GetWords()
+            ));
+
+
+
+
+
